c290a8759a1c36621f07eaaf31d28ceb1de3caca
ThemeOri <124379572+ThemeOri@users.noreply.github.com>
2023-02-28 16:16:07 +0600

    Update process.php

--- process.php
+++ process.php
@@ -1,25 +1,6 @@
-<?php
-class Person {
-	private $name;
-	private $email;
-	
-	public function setName($name) {
-		$this->name = $name;
-	}
-	
-	public function setEmail($email) {
-		$this->email = $email;
-	}
-	
-	public function getName() {
-		return $this->name;
-	}
-	
-	public function getEmail() {
-		return $this->email;
-	}
-}
+<?php 
 
+include 'Person.php'; 
 
 $person = new Person();
 $person->setName('John Doe');
